b89b8350ecc8796034dc015b4bca60324735d1b6
igorcamposcode <igorcampos_45@yahoo.com>
2024-06-19 15:12:10 -0300

    Adicionando o formulário para fazer a inclusão das tarefas na tabela (Protótipo de Inclusão de Tarefas)

--- src/app/mes/mes.component.ts
+++ src/app/mes/mes.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-mes',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule,MatSelectModule,MatInputModule,FormsModule,ReactiveFormsModule],
   templateUrl: './mes.component.html',
   styleUrl: './mes.component.css'
 })
 export class MesComponent {
+[x: string]: any;
+
+  tarefaFormControl = new FormControl('',[Validators.required]);
+
+  constructor(private router: Router) {}
+
+  // Retorna para a página Menu //
+
+  VoltarMenu(pageName: string){
+    this.router.navigate([`${pageName}`]);
+  }
 
 }
